6fbcf2eb84846ec2a15d85f64d0483891e9b919b
eugrodmar <eugrodmar@gmail.com>
2025-12-17 10:57:30 +0100

    Primer commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const cantidadSelect = document.getElementById('cantidad');
+const desplegablesContainer = document.getElementById('desplegable-container');
+
+cantidadSelect.addEventListener('change', function(){
+
+});
